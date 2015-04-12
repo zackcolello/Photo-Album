34ba8922a8d59193b377defdd213acb689ed7547
zackcolello <zackcolello@localhost>
2015-04-11 23:08:27 -0400

    Finished GUI for state 7

--- src/cs213/photoAlbum/GUIViewStates/PhotoAlbumStore.java
+++ src/cs213/photoAlbum/GUIViewStates/PhotoAlbumStore.java
@@ -5,8 +5,10 @@ public class PhotoAlbumStore {
 	static final Login1State login1State = Login1State.getInstance();
 	static final Admin2State admin2State = Admin2State.getInstance();
 	static final Album3State album3State = Album3State.getInstance();
-	static final InAlbum4State inalbum4State= InAlbum4State.getInstance();
+	static final InAlbum4State inalbum4State = InAlbum4State.getInstance();
 	static final Album5State album5State = Album5State.getInstance();
+	static final SearchTag7State searchTag7State = SearchTag7State
+			.getInstance();
 
 	static final SearchDate6State searchDate6State = SearchDate6State
 			.getInstance();
